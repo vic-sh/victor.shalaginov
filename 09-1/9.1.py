a73678b8230e2d8a43c5732641a974168e912f92
Victor Shalaginov <victor.shalaginov@gmail.com>
2019-01-24 15:54:30 +0300

    9.1 done second commit

--- 09-1/9.1.py
+++ 09-1/9.1.py
@@ -1,4 +1,7 @@
+access_config = []
+
 def generate_access_config(access):
+
     '''
     access - словарь access-портов,
     для которых необходимо сгенерировать конфигурацию, вида:
@@ -14,6 +17,21 @@ def generate_access_config(access):
         'spanning-tree bpduguard enable'
     ]
 
+    for interface in access_dict.keys():
+        vlan = access_dict.get(interface)
+
+        print(interface)
+        access_config.append(interface)
+
+        for string in access_template:
+            if string.endswith('access vlan'):
+                print(' ' + string + ' ' + str(vlan))
+                access_config.append(' ' + string + ' ' + str(vlan))
+            else:
+                print(' ' + string)
+                access_config.append(' ' + string)
+
+    return access_config
 
 
 access_dict = {
@@ -23,3 +41,6 @@ access_dict = {
     'FastEthernet0/17': 150
 }
 
+generate_access_config(access_dict)
+
+print(access_config)
