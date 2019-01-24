67fe63667d35b23fff9bab73b33614cde9138c52
Victor Shalaginov <victor.shalaginov@gmail.com>
2019-01-24 15:50:10 +0300

    9.1 done

---
+++ 09-1/scratch.py
@@ -0,0 +1,25 @@
+def generate_access_config(access):
+    '''
+    access - словарь access-портов,
+    для которых необходимо сгенерировать конфигурацию, вида:
+        { 'FastEthernet0/12':10,
+          'FastEthernet0/14':11,
+          'FastEthernet0/16':17}
+
+    Возвращает список всех портов в режиме access с конфигурацией на основе шаблона
+    '''
+    access_template = [
+        'switchport mode access', 'switchport access vlan',
+        'switchport nonegotiate', 'spanning-tree portfast',
+        'spanning-tree bpduguard enable'
+    ]
+
+
+
+access_dict = {
+    'FastEthernet0/12': 10,
+    'FastEthernet0/14': 11,
+    'FastEthernet0/16': 17,
+    'FastEthernet0/17': 150
+}
+
