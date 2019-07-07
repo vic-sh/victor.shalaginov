c48cce65bb982186d97410661a2954adea9a69ad
Victor Shalaginov <victor.shalaginov@gmail.com>
2019-07-07 18:28:38 +0300

    15.4 added

---
+++ 15/15.4.py
@@ -0,0 +1,30 @@
+import argparse, re
+from pprint import pprint
+
+parser = argparse.ArgumentParser(description='Fileparser IP address + Mask')
+parser.add_argument('filename', action="store", help="Enter a name of file to parse")
+#parser.add_argument('regex', action="store", help="Enter a regex to parse in the file")
+
+args = parser.parse_args()
+print(args)
+
+regex = ('(\S+) +'
+         '([\d.])+ +'
+         '\w+ +\w+ +'
+         '(up|down|administratively down) +'
+         '(up|down)')
+
+def parse_cfg(filename):
+
+    result = []
+
+    with open(filename) as f:
+        match_iter = re.finditer(regex, f.read())
+        for match in match_iter:
+            result.append(match.groups())
+
+    return result
+
+result2 = parse_cfg(args.filename)
+pprint(result2)
+
