87ef30f4006e853826a37113fdb83bcae8f17d09
Victor Shalaginov <victor.shalaginov@gmail.com>
2019-08-30 17:24:28 +0300

    17.1 todo_modify_regex

--- 17/17.1.py
+++ 17/17.1.py
@@ -6,24 +6,34 @@ sh_version_files = glob.glob('sh_vers*')
 headers = ['hostname', 'ios', 'image', 'uptime']
 
 output = ''
+match = ''
+output_from_the_file_stripped=''
 
 #regex = ('.+\), Version (\S+),.+'
 #         'router uptime is (\S+ days, \S+ hours, \S+ minutes).+'
 #         'System image file is "(\S+)"')
 
+regex = ('.+\), Version (\S+),.+'
+         'router uptime is (\S+ days, \S+ hours, \S+ minutes)')
+
+#regex = ('.+\), Version (\S+) .+(\s.+)+router uptime is (\S+ days, \S+ hours, \S+ minutes)')
+
 filename = "sh_version_r1.txt"
 
 with open(filename) as f:
     output_from_the_file = f.read()
+    output_from_the_file_stripped = output_from_the_file.strip()
 
 def parse_sh_version(output):
     result = []
-    match_iter = re.search(regex, output)
+    match = re.search(regex, output)
+    print(match.groups())
 
 #    for match in match_iter:
 #        result.append(match.groups())
-#    return result
+    return match
 
-result = parse_sh_version(output_from_the_file)
-print(result)
+result = parse_sh_version(output_from_the_file_stripped)
+
+print(match)
 
