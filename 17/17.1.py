cdd23ac8298663ed9eb6bf8a85dd3b71e606b2a3
Victor Shalaginov <victor.shalaginov@gmail.com>
2019-08-30 12:11:07 +0300

    17.1 modify_to_search

--- 17/17.1.py
+++ 17/17.1.py
@@ -7,16 +7,23 @@ headers = ['hostname', 'ios', 'image', 'uptime']
 
 output = ''
 
-regex = ('Cisco IOS Software, \w+  , Version (\S+)'
-         'router uptime is (\w+)'
-         'System image file is "(\w+)"')
+#regex = ('.+\), Version (\S+),.+'
+#         'router uptime is (\S+ days, \S+ hours, \S+ minutes).+'
+#         'System image file is "(\S+)"')
 
 filename = "sh_version_r1.txt"
 
 with open(filename) as f:
-    output = f.read()
-    print(output)
+    output_from_the_file = f.read()
 
 def parse_sh_version(output):
+    result = []
+    match_iter = re.search(regex, output)
 
-    pass
+#    for match in match_iter:
+#        result.append(match.groups())
+#    return result
+
+result = parse_sh_version(output_from_the_file)
+print(result)
+
