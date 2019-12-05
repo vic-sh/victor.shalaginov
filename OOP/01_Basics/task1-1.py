4d7f013773fc4b3ec84f318c441eea6de0f29e27
Victor Shalaginov <victor.shalaginov@gmail.com>
2019-12-05 15:27:34 +0300

    task1-1 updated ip_network ok

--- OOP/01_Basics/task1-1.py
+++ OOP/01_Basics/task1-1.py
@@ -1,11 +1,15 @@
-class IPv4Network:
+import ipaddress
+
+class IPv4Network(object):
+
     def __init__(self, ipv4network):
-        self.ipv4network = ipv4network
-        self.address = ''
-        self.mask = ''
-        self.broadcast = ''
-        self.allocated = ()
+        self.ipv4network = ipaddress.ip_interface(ipv4network)
+        self.address = ipaddress.ip_interface(ipv4network).ip
+        self.mask = ipaddress.ip_interface(ipv4network).network
+        self.broadcast = ipaddress.ip_interface(ipv4network).network.broadcast_address
+        self.allocated = list(ipaddress.ip_interface(ipv4network).network.hosts())
 
     def hosts(self):
         self.ipv4network
+
 
