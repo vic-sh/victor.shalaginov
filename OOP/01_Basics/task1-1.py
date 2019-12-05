b2ea8e59ce907891886ae48f42f13e22d9d9003f
Victor Shalaginov <victor.shalaginov@gmail.com>
2019-12-05 10:52:25 +0300

    OOP started task1-1 updated

--- OOP/01_Basics/task1-1.py
+++ OOP/01_Basics/task1-1.py
@@ -1,2 +1,11 @@
 class IPv4Network:
-    pass
+    def __init__(self, ipv4network):
+        self.ipv4network = ipv4network
+        self.address = ''
+        self.mask = ''
+        self.broadcast = ''
+        self.allocated = ()
+
+    def hosts(self):
+        self.ipv4network
+
