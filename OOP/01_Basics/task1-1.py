bbbeb3251da22d5996f19cce2bae50d7b91ebf81
Victor Shalaginov <victor.shalaginov@gmail.com>
2019-12-06 17:27:32 +0300

    task1-1 everything ok except unassigned

--- OOP/01_Basics/task1-1.py
+++ OOP/01_Basics/task1-1.py
@@ -4,12 +4,21 @@ class IPv4Network(object):
 
     def __init__(self, ipv4network):
         self.ipv4network = ipaddress.ip_interface(ipv4network)
-        self.address = ipaddress.ip_interface(ipv4network).ip
-        self.mask = ipaddress.ip_interface(ipv4network).network
-        self.broadcast = ipaddress.ip_interface(ipv4network).network.broadcast_address
-        self.allocated = list(ipaddress.ip_interface(ipv4network).network.hosts())
+        self.address = str(ipaddress.ip_interface(ipv4network).ip)
+        self.mask = ipv4network.split('/')[1]
+        self.broadcast = str(ipaddress.ip_interface(ipv4network).network.broadcast_address)
+        self.allocated_list = []
+        self.allocated = tuple(self.allocated_list)
+        self.unassigned = list(ipaddress.ip_interface(ipv4network).network.hosts())
 
     def hosts(self):
-        self.ipv4network
+        for ip in ipaddress.ip_interface(self.ipv4network).network.hosts():
+            print(ip)
+
+    def allocate(self, allocated_ip_address):
+        self.allocated_list.append(allocated_ip_address)
+        self.allocated = tuple(self.allocated_list)
+        #self.unassigned = self.unassigned.remove(allocated_ip_address)
+
 
 
