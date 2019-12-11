34014ebbbdc327c1e238044ce0d7cc7d7951debe
Victor Shalaginov <victor.shalaginov@gmail.com>
2019-12-11 17:09:53 +0300

    task1-1 done

--- OOP/01_Basics/task1-1.py
+++ OOP/01_Basics/task1-1.py
@@ -1,7 +1,6 @@
 import ipaddress
 
 class IPv4Network(object):
-
     def __init__(self, ipv4network):
         self.ipv4network = ipaddress.ip_interface(ipv4network)
         self.address = str(ipaddress.ip_interface(ipv4network).ip)
@@ -9,16 +8,18 @@ class IPv4Network(object):
         self.broadcast = str(ipaddress.ip_interface(ipv4network).network.broadcast_address)
         self.allocated_list = []
         self.allocated = tuple(self.allocated_list)
-        self.unassigned = list(ipaddress.ip_interface(ipv4network).network.hosts())
-
+        self.unassigned_list = []
+        for ip in ipaddress.ip_interface(self.ipv4network).network.hosts():
+            self.unassigned_list.append(str(ip))
+        self.unassigned = tuple(self.unassigned_list)
     def hosts(self):
         for ip in ipaddress.ip_interface(self.ipv4network).network.hosts():
             print(ip)
-
     def allocate(self, allocated_ip_address):
         self.allocated_list.append(allocated_ip_address)
         self.allocated = tuple(self.allocated_list)
-        #self.unassigned = self.unassigned.remove(allocated_ip_address)
+        self.unassigned_list.remove(allocated_ip_address)
+        self.unassigned = tuple(self.unassigned_list)
 
 
 
