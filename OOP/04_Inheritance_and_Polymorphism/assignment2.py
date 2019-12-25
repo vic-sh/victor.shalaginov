3336476dfbfa830dadc9b3fa42335c8185b25eb2
Victor Shalaginov <victor.shalaginov@gmail.com>
2019-12-25 17:06:55 +0300

    telegram caller bot added2

--- OOP/04_Inheritance_and_Polymorphism/assignment2.py
+++ OOP/04_Inheritance_and_Polymorphism/assignment2.py
@@ -2,12 +2,15 @@ import datetime
 
 class WriteFile(object):
     def __init__(self, filename):
-        pass
+        self.filename = filename
+    def writetofile(self, filename, linetowrite):
+        with open(self.filename) as file:
+            file.write(linetowrite)
 
 class LogFile(WriteFile):
-    def write(self, string_to_write):
+    def form_string_towrite(self, string_to_write):
         self.dt_str = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
-        
+
 
 class DelimFile(WriteFile):
     pass
---
+++ telegram_caller_bot/config.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+token = '419133999:AAHVHi24clyKLUSTi7v39Y0gV6_OzXbTGT4'
+
